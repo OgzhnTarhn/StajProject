9c8ca572fd3c3cf3710656b913c9d384110187b4
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-07-28 13:59:18 +0300

    Register sayfası iyileştirildi SAP'den düzgün bir şekilde kullanıcı insert yapabiliyoruz

--- StajProject/Models/RegisterModel.cs
+++ StajProject/Models/RegisterModel.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace StajProject.Models
+﻿namespace StajProject.Models
 {
-    public class Class1
+    public class RegisterModel
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }      // Sadece admin panelinde kullanılacak!
+        public string Message { get; set; }   // Sonuç mesajı (SAP'den gelen)
     }
 }
