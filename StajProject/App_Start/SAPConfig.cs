cca44115ae06af007406ced862d5b704f69673db
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-07-29 08:48:37 +0300

    User Dashboard is fixed

--- StajProject/App_Start/SAPConfig.cs
+++ StajProject/App_Start/SAPConfig.cs
@@ -12,14 +12,13 @@ public class SAPConfig : IDestinationConfiguration
         {
             RfcConfigParameters parms = new RfcConfigParameters();
             parms.Add(RfcConfigParameters.Name, "SAP_DEST");
-            parms.Add(RfcConfigParameters.AppServerHost, "sapserver.domain.com"); // ← kendi SAP server'ını gir
+            parms.Add(RfcConfigParameters.AppServerHost, "172.31.2.10");
             parms.Add(RfcConfigParameters.SystemNumber, "00");
-            parms.Add(RfcConfigParameters.User, "SAPUSER");
-            parms.Add(RfcConfigParameters.Password, "password");
+            parms.Add(RfcConfigParameters.User, "xdeveloper");
+            parms.Add(RfcConfigParameters.Password, "Sf5687!pl@");
             parms.Add(RfcConfigParameters.Client, "100");
             parms.Add(RfcConfigParameters.Language, "EN");
             parms.Add(RfcConfigParameters.PoolSize, "5");
-            return parms;
         }
         return null;
     }
