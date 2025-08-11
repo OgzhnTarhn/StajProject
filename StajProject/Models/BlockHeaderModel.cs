602958841784b8adb463c640df24532c9e6a76f1
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-08-11 13:41:58 +0300

    Fixing index.cshtml

--- StajProject/Models/BlockHeaderModel.cs
+++ StajProject/Models/BlockHeaderModel.cs
@@ -5,7 +5,7 @@
         public string Mandt { get; set; }
         public string BlockId { get; set; }
         public string Title { get; set; }
-        public string Erdat { get; set; }
-        public string Aedat { get; set; }
+        public string Erdat { get; set; }   // DATS
+        public string Aedat { get; set; }   // DATS
     }
 }
